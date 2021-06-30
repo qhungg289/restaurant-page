a43b608fe3235d70f9ab027091494f42a0eb87d5
Hung Duong Quang <qhungg289@protonmail.com>
2021-06-30 21:10:25 +0700

    🔧 Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,20 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
-    devtool: 'inline-source-map'
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
 };
