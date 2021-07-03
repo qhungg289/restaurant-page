6d6858af895ba1d845a0278480037bfc88e628bf
Hung Duong Quang <qhungg289@protonmail.com>
2021-07-03 22:03:54 +0700

    🔧 Add tab switching logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,44 @@
 import "./style.css"
+import showHeader from "./header"
+import showHome from "./home"
+import showMenu from "./menu"
+import showContact from "./contact"
 
-import homeDisplay from "./home"
+// Create & get the barebone elements
+let content = document.getElementById("content");
+let main = document.createElement("div");
+main.classList.add("main");
+main.id = "main";
 
-homeDisplay();
+// Init the contents
+content.appendChild(showHeader());
+content.appendChild(main);
+showHome();
+
+// Tab switching logic
+let navItems = document.getElementsByClassName("nav-link")
+navItems[0].classList.add("active");
+
+navItems[0].onclick = () => {
+    document.getElementById("main").innerHTML = "";
+    navItems[0].classList.add("active");
+    navItems[1].classList.remove("active");
+    navItems[2].classList.remove("active");
+    showHome();
+}
+
+navItems[1].onclick = () => {
+    document.getElementById("main").innerHTML = "";
+    navItems[1].classList.add("active");
+    navItems[0].classList.remove("active");
+    navItems[2].classList.remove("active");
+    showMenu();
+}
+
+navItems[2].onclick = () => {
+    document.getElementById("main").innerHTML = "";
+    navItems[2].classList.add("active");
+    navItems[0].classList.remove("active");
+    navItems[1].classList.remove("active");
+    showContact();
+}
