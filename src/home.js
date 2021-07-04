cbfe16d0827443d65d6c39639d753e7d0251c508
Hung Duong Quang <qhungg289@protonmail.com>
2021-07-04 22:54:24 +0700

    Add fade in animation and order button

--- src/home.js
+++ src/home.js
@@ -6,8 +6,8 @@ export default function showHome() {
     // Sample image
     let sampleImg = new Image();
     sampleImg.src = myImage;
-    sampleImg.alt = "Sample";
-    sampleImg.classList.add("home-sample")
+    sampleImg.classList.add("home-sample");
+    sampleImg.classList.add("animate-fading");
 
     // Headline
     let headline = document.createElement("h2");
@@ -18,10 +18,16 @@ export default function showHome() {
     p.innerHTML = `With the ingredients straight from our organic farm, 
                    we're proud to deliver the most delicious meal for you and the one you love the most.`
 
+    // Button
+    let orderButton = document.createElement("button");
+    orderButton.innerHTML = "ORDER NOW";
+
     let textContainer = document.createElement("div");
     textContainer.classList.add("text-container");
+    textContainer.classList.add("animate-fading");
     textContainer.appendChild(headline);
     textContainer.appendChild(p);
+    textContainer.appendChild(orderButton);
 
     // Append all elements to the main section
     main.appendChild(sampleImg);
