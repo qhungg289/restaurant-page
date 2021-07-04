3c60c60faafae2b39260142afab9bb6e04cdfb2f
Hung Duong Quang <qhungg289@protonmail.com>
2021-07-04 22:52:22 +0700

    Add content in contact page

--- src/contact.js
+++ src/contact.js
@@ -1,8 +1,24 @@
+import location from "./location.png"
+
 export default function showContact() {
     let main = document.getElementById("main");
 
-    let p = document.createElement("p");
-    p.innerHTML = "Testing contact";
+    let contactContainer = document.createElement("div");
+    contactContainer.classList.add("contact");
+    contactContainer.classList.add("animate-fading");
 
-    main.appendChild(p);
+    let address = new Image();
+    address.src = location;
+
+    let telephone = document.createElement("p");
+    telephone.innerHTML = "<b>Telephone:</b> 0000 1111 2222";
+
+    let email = document.createElement("p");
+    email.innerHTML = "<b>Email:</b> tsuki@gmail.com";
+
+    contactContainer.appendChild(address);
+    contactContainer.appendChild(telephone);
+    contactContainer.appendChild(email);
+
+    main.appendChild(contactContainer);
 }
